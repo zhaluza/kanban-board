afd542f12ec3c4e9d9605439ee8f30d637795b4b
Zac H <zac.haluza@gmail.com>
2020-07-04 21:57:25 -0400

    React & webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
